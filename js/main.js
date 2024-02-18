2c203bdd7e062cbf8a3015aafac49ec561424465
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-02-18 17:12:44 -0300

    Todo o avanço dessas 2 semanas que eu esqueci de dar commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$('.slider-principal').slick();
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 3000,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
