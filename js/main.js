6880ca795fdfdbe02332d10c959df3818a40e53b
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-02-18 21:00:41 -0300

    final do projeto

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $('.slider-principal').slick({
     dots: true,
     infinite: true,
-    speed: 3000,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
 });
